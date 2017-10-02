f6ef88c8efc61f75ef0de98b0283825c069f7daf
MarauderPixie <tobias.anton.85@gmail.com>
2017-10-02 10:58:49 +0200

    first attempt at OAuth tokenification

---
+++ OAuth.R
@@ -0,0 +1,19 @@
+# die kann ich einfach bei meiner App nachschauen
+client_id     <- "xxxxxxxxxxxxxxxxxxxxxxxxx"
+client_secret <- "yyyyyyyyyyyyyyyyyyyyyyyyy"
+
+# damit bekomm ich offenbar ein cooles oAuth-Token, das ich ganz viel weiter verwenden kann
+# das ganze ist stumpf geklaut von https://github.com/tiagomendesdantas/Rspotify
+spotifyOAuth <- function(app_id, client_id, client_secret){
+  spotifyR <- httr::oauth_endpoint(
+    authorize = "https://accounts.spotify.com/authorize",
+    access    = "https://accounts.spotify.com/api/token"
+    )
+  # "R Web Scraper" ist der name meiner App; die app_id also
+  myapp <- httr::oauth_app("R Web Scraper", client_id, client_secret)
+  return(httr::oauth2.0_token(spotifyR, myapp,scope = "user-top-read"))}
+
+
+tokn <- spotifyOAuth("", client_id, client_secret)
+
+tokn_me <- spotifyOAuth("", client_id, client_secret)
