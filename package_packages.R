7488a53ec6d30431e106e6052bb2f515dfb5e853
MarauderPixie <tobias.anton.85@gmail.com>
2017-10-02 10:59:43 +0200

    added homebrewn ggplot-theme

--- package_packages.R
+++ package_packages.R
@@ -3,3 +3,7 @@ library(tidyverse)
 library(httr)
 library(stringr)
 library(lubridate)
+
+# visuals
+source("https://tobi.tadaa-data.de/theme_tut.R")
+theme_set(theme_tut(style = "light", legend = "right"))
